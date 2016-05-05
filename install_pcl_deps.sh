71f2655ee9318d30790dbd474279bc94822b02b5
Ningxin Hu <ningxin.hu@intel.com>
2016-05-05 14:57:34 +0800

    Remove temp folder after installation

--- install_pcl_deps.sh
+++ install_pcl_deps.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
+# Install ubuntu packages
 sudo apt-get update
-
 sudo apt-get install libflann-dev libvtk5-dev libboost-all-dev libusb-1.0-0-dev libqhull-dev libpcap0.8-dev
 
+# Install metslib 0.5.3
 wget http://www.coin-or.org/download/source/metslib/metslib-0.5.3.tgz
 tar zxvf metslib-0.5.3.tgz
 cd metslib-0.5.3/
@@ -10,7 +11,9 @@ cd metslib-0.5.3/
 make -j9
 sudo make install
 cd ..
+rm -rf metslib-0.5.3*
 
+# Install eigen 3.1.4
 hg clone https://bitbucket.org/eigen/eigen/
 cd eigen
 hg up 3.1.4
@@ -19,3 +22,6 @@ cd builddir
 cmake ../
 sudo make install
 cd ../..
+rm -rf eigen
+
+echo "Done installing pcl deps"
